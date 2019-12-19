d2eda1a67fc9fc0516423c064a1ad2d15ebd51ce
huihuiboy <55218458+huihuiboy@users.noreply.github.com>
2019-12-19 12:09:12 +0800

    開門ㄌ

---
+++ TUMT_VR/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static bool havKey;
+
+    //公開且參數只能零或一個
+    public void Throwing(GameObject obj)
+    {
+        print("Throw");
+        havKey = true;
+        Destroy(obj);
+    }
+    public void Taking()
+    {
+        print("Taking");
+
+    }
+    public void Take()
+    {
+        print("Take");
+    }
+
+
+}
---
+++ TUMT_VR/Assets/teleblablabla.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class teleblablabla : MonoBehaviour {
+
+    [Header("動畫控制器")]
+    public Animator Con;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "HeadCollider" &&  GameManager.havKey)
+        {
+            Con.SetBool("character_nearby", true);
+            
+        }
+    }
+}
