e4d3ab0d30e2b9dabf93bd311e06d79c87f06e59
huihuiboy <55218458+huihuiboy@users.noreply.github.com>
2020-01-07 11:39:58 +0800

    =yee

---
+++ TUMT_VR/Assets/script/openDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour {
+
+    public Animator ani;
+   
+
+	// Use this for initialization
+	void Start () {
+        ani = gameObject.GetComponent<Animator>();
+	}
+
+    public void OpenDoor()
+    {
+        ani.SetBool("character_nearby", true);
+        Destroy(this);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
